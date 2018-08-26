809b5271127cc361190e49c48e57c3918e53c051
therafk <therafk@hotmail.com>
2018-08-26 19:29:10 +0100

    first few lines

--- templateA1.sh
+++ templateA1.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 
+#https://www.youtube.com/watch?v=zWVV31NYi1U
+
+echo "Hi there"
+# ==== read user input ====
+echo "Enter names "
+read name1 name2 name3
+echo "Names are: ${name1}, ${name2}, ${name3}"
+
+read -p "Enter name: "
+read -sp "Enter Password: "
+# --- read an array ---
+echo "Enter names:"
+read -a names
+echo "Names are: ${names[0], $names[1]}"
+# --- build in variable REPLY -- 
+echo "Enter name: "
+read
+echo "Name: $REPLY"
+
+# === pas argument to a script ===
